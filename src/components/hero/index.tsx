d76d2a3f41ad54fe686544bbf63bb86b1b702d40
Stackbit Code Editor <projects@stackbit.com>
2021-10-26 17:30:08 +0000

    index.tsx: changed

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Markdown from 'markdown-to-jsx';
 import classNames from 'classnames';
 import { getComponent } from '@stackbit/components';
-import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/';
+import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/components-registry';
 
 export default function HeroSection(props) {
     const colors = props.colors || 'colors-a';
